8a3449dff2a30de980198e80acd28b272e5e9b0d
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-05-13 15:00:46 +0700

    feat: Style Check + FE Workflow
    - Added a yaml file defining the github workflow that only triggers by the frontend
    
    - Added eslint and prettier config to unify the styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 const Page = () => {
-  return <div>Empty</div>;
-};
+	return <div>Empty</div>
+}
 
-export default Page;
+export default Page
