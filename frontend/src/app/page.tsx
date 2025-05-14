814ed9704cd55ecda6ac8aad7d17b1efc416193c
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2025-05-14 08:11:00 +0700

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Page = () => {
-	return <div>Empty with test 12345</div>
+	return <div>Empty with test 123456 Con Bin Tuc No Bi J Ay</div>
 }
 
 export default Page
