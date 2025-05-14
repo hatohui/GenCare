08cad8795150a097b40739c1da359b1e7816a66f
minebinroyal1@gmail.com <minebinroyal1@gmail.com>
2025-05-14 23:47:15 +0700

    Added simple registerForm using zod as schema

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
 import LoginForm from '@/Components/Auth/LoginForm'
+import RegisterPage from '@/Components/Auth/RegisterForm'
 
 const page = () => {
-	return <LoginForm />
+	// return <LoginForm />
+	return <RegisterPage />
 }
 
 export default page
