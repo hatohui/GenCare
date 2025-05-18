b62382df5bc5bc430a2b0ce425e2c83a2f340269
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-05-19 04:29:56 +0700

    fix: wierd main & lockfile error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,9 @@
 'use client'
 
-// import LoginForm from '@/Components/Auth/LoginForm'
 import RegisterPage from '@/Components/Auth/RegisterForm'
 
 const Page = () => {
-	return <div>Empty 123456789</div>
-const page = () => {
 	return <RegisterPage />
 }
 
 export default Page
-
-//uptate something
-export default page
