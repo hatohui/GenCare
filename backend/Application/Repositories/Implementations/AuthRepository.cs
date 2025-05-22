cef58778da09920c78f449042568afab8771a6b2
Phạm Anh Kiệt <phamanhkiet.dev@gmail.com>
2025-05-22 11:18:32 +0700

    feat: implement AuthRepository and update service collection to include IAuthRepository

---
+++ backend/Application/Repositories/Implementations/AuthRepository.cs
@@ -0,0 +1,37 @@
+﻿using Application.Repositories.Interfaces;
+using Domain.Entities;
+
+namespace Application.Repositories.Implementations;
+
+public class AuthRepository : IAuthRepository
+{
+    Task<Guid> IAuthRepository.AddUserAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<User?> IAuthRepository.GetByEmailAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<User?> IAuthRepository.GetByPhoneNumberAsync(string phoneNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<User?> IAuthRepository.GetByRefreshTokenAsync(string refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task IAuthRepository.UpdateRefreshTokenAsync(int userId, string refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task IAuthRepository.UpdateUserAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
