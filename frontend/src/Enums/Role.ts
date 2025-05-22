6c1c86eb4020a007ce824bc6eba551c7ac043e26
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2025-05-22 17:24:23 +0700

    chore: added boilerplate for frontend repo
    #skip

---
+++ frontend/src/Enums/Role.ts
@@ -0,0 +1,4 @@
+export enum Role {
+	Staff = 0,
+	Member = 1,
+}
--- frontend/src/Interfaces/Auth/API/register.type.ts
+++ frontend/src/Interfaces/Auth/API/register.type.ts
@@ -1,6 +1,18 @@
 import { z } from 'zod'
+import { Account } from '../Types/Account'
 
-export const userSchema = z
+export type RegisterFormData =
+	| Pick<
+			Account,
+			'firstName' | 'lastName' | 'dateOfBirth' | 'email' | 'address'
+	  > & {
+			password: string
+			confirmPassword: string
+			agreeToTerms: boolean
+			timestamp: Date | string
+	  }
+
+export const RegisterSchema = z
 	.object({
 		firstName: z.string().min(1, { message: 'First name is required' }),
 		lastName: z.string().min(1, { message: 'Last name is required' }),
@@ -10,10 +22,6 @@ export const userSchema = z
 			.min(1, { message: 'Date of birth is required' }),
 		email: z.string().email({ message: 'Email is not valid' }),
 		address: z.string().min(1, { message: 'Address is required' }),
-
-		username: z
-			.string()
-			.min(3, { message: 'Username must be at least 3 characters long' }),
 		password: z
 			.string()
 			.min(6, { message: 'Password must be at least 6 characters long' }),
