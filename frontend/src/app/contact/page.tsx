faf1342bb7f6b3caee51e2c06380b22608126cc9
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2025-05-24 08:57:36 +0700

    Update page.tsx

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,13 +1,8 @@
 'use client'
-import Loading from '@/Components/Loading'
-import { stimulateLoad } from '@/Utils/LoadStimulate'
-import React, { useState } from 'react'
+import React from 'react'
 
 const page = () => {
-	const [isLoading, setIsLoading] = useState(true)
-	stimulateLoad(5000)
-
-	return isLoading ? <Loading /> : <div></div>
+	return <div></div>
 }
 
 export default page
