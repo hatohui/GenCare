96f34d6823ab12d9060254ea92684e4c132543fa
minebinroyal1@gmail.com <minebinroyal1@gmail.com>
2025-05-27 01:10:16 +0700

    feat: loading circle

--- frontend/src/Components/Loading.tsx
+++ frontend/src/Components/Loading.tsx
@@ -1,24 +1,17 @@
 'use client'
 
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 
 export default function LoadingPage() {
 	return (
-		<div className='fixed top-0 left-0 w-full h-1 z-[9999]'>
+		<div className='fixed top-0 left-0 w-full h-full flex items-center justify-center z-[9999]'>
 			<motion.div
-				className='h-full bg-main'
-				initial={{ width: 0 }}
-				animate={{ width: '100%' }}
+				className='w-[50px] h-[50px] rounded-full border-4 border-solid border-divider border-t-accent spinner'
+				animate={{ rotate: 360 }}
 				transition={{
-					duration: 2,
-					ease: 'easeInOut',
+					duration: 1.5,
 					repeat: Infinity,
-					repeatType: 'loop',
-				}}
-				variants={{
-					animate: {
-						width: ['0%', '100%'],
-					},
+					ease: 'linear',
 				}}
 			/>
 		</div>
