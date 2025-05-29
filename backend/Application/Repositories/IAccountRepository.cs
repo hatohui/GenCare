561a64c119b0c953799da430d80a0a3682e4d028
Hồng Lê Đăng Khoa <hongkhoa348@gmail.com>
2025-05-29 18:06:55 +0700

    feat: add login

--- backend/Application/Repositories/IAccountRepository.cs
+++ backend/Application/Repositories/IAccountRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace Application.Repositories;
+﻿using Domain.Entities;
+
+namespace Application.Repositories;
 
 public interface IAccountRepository
 {
+ Task<Account?> GetAccountByEmailPasswordAsync(string email, string password);
 
 }
--- backend/Application/Services/IAccountService.cs
+++ backend/Application/Services/IAccountService.cs
@@ -1,5 +1,10 @@
-﻿namespace Application.Services;
+﻿using Application.DTOs.Auth.Requests;
+using Application.DTOs.Auth.Responses;
+
+
+namespace Application.Services;
 
 public interface IAccountService
 {
+    Task<UserLoginResponse?> LoginAsync(UserLoginRequest request);
 }
