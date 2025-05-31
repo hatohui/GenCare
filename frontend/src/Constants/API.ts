acd0d15a59ec2ce6d39ecb225684eadd97d60fed
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-06-01 06:02:35 +0700

    chore: forgot to change API route back

--- frontend/src/Constants/API.ts
+++ frontend/src/Constants/API.ts
@@ -1,2 +1,2 @@
-// export const DEFAULT_API_URL = 'https://api.gencare.site/api'
-export const DEFAULT_API_URL = 'http://localhost:8080/api'
+export const DEFAULT_API_URL = 'https://api.gencare.site/api'
+// export const DEFAULT_API_URL = 'http://localhost:8080/api'
