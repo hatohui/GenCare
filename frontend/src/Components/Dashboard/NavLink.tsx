e69466f80b927c27d63f4f90c6d7cbe103003189
Đinh Gia Huy <minebinroyal1@gmail.com>
2025-06-02 23:26:31 +0700

    fix: fixing abit of thing

--- frontend/src/Components/Dashboard/NavLink.tsx
+++ frontend/src/Components/Dashboard/NavLink.tsx
@@ -7,16 +7,21 @@ import { SIDE_NAV_OPTIONS as links } from '@/Constants/SideNav'
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
+interface NavLink {
+	label: string
+	to: string
+}
 
 export default function NavLinks() {
 	const pathname = usePathname()
 	return (
 		<>
-			{links.map(link => {
+			{links.map((link: NavLink) => {
 				return (
 					<Link
 						key={link.label}
 						href={link.to}
+						aria-current={pathname === link.to ? 'page' : undefined}
 						className={clsx(
 							'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-accent md:flex-none md:justify-start md:p-2 md:px-3',
 							{
@@ -24,7 +29,7 @@ export default function NavLinks() {
 							}
 						)}
 					>
-						<p className=' md:block'>{link.label}</p>
+						<span className='md:block'>{link.label}</span>
 					</Link>
 				)
 			})}
--- frontend/src/Components/Dashboard/Sidenav.tsx
+++ frontend/src/Components/Dashboard/Sidenav.tsx
@@ -1,7 +1,14 @@
 import Link from 'next/link'
 import NavLinks from './NavLink'
+import { useRouter } from 'next/navigation'
+import { clearAccessToken } from '@/Utils/clearAccessToken'
 
 export default function SideNav() {
+	const router = useRouter()
+	const handleLogout = () => {
+		clearAccessToken()
+		router.push('/login')
+	}
 	return (
 		<div className='flex h-full flex-col px-4 py-4 md:px-2'>
 			<Link
@@ -14,7 +21,10 @@ export default function SideNav() {
 				<NavLinks />
 				<div className='hidden h-auto w-full grow rounded-md bg-gray-50 md:block'></div>
 				<form>
-					<button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
+					<button
+						onClick={handleLogout}
+						className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'
+					>
 						<div className='hidden md:block'>Sign Out</div>
 					</button>
 				</form>
