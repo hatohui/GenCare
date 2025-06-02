bc854ea485930801a0b53011001760f51ff739dc
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-06-02 21:16:52 +0700

    feat: added token expiration check

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,18 +1,42 @@
-import { ACCESS_TOKEN_COOKIE_STRING } from '@/Constants/Auth'
-import { getCookie } from 'cookies-next/client'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { getDecodedToken } from './Utils/getDecodedToken'
+import { accountActions } from './Hooks/useToken'
 
 export function middleware(request: NextRequest) {
-	const token = getCookie(ACCESS_TOKEN_COOKIE_STRING)
+	try {
+		const rawClaim = getDecodedToken()
+		if (!rawClaim) throw new Error('No session found.')
+		// Check if token is expired
+		if (rawClaim.exp && typeof rawClaim.exp === 'number') {
+			//get currenttime in seconds
+			const currentTimestamp = Math.floor(Date.now() / 1000)
+			//compare
+			if (rawClaim.exp < currentTimestamp) {
+				throw new Error('Token has expired')
+			}
+		}
+	} catch (error) {
+		//handle token expiration
+		const errorMessage =
+			error instanceof Error ? error.message : 'Unknown error'
 
-	if (!token) {
-		return NextResponse.redirect(new URL('/login', request.url))
+		console.error('Authentication error:', errorMessage)
+
+		if (
+			errorMessage.includes('Token has expired') ||
+			errorMessage.includes('No session found')
+		) {
+			accountActions.removeAccount()
+			return NextResponse.redirect(new URL('/login', request.url))
+		}
+
+		return NextResponse.redirect(new URL('/error', request.url))
 	}
 
 	return NextResponse.next()
 }
 
 export const config = {
-	matcher: ['/dashboard/:path*'],
+	matcher: ['/dashboard/:path*'], //add more here later
 }
