db29d5cbd108dd80534981e65b5ab7930f72637a
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2025-06-04 14:07:34 +0700

    feat: itemCards

---
+++ frontend/src/Components/Management/SearchBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export type SearchBarProps = {}
+
+const SearchBar = () => {
+	return <div className='bg-accent min-h-3 w-full h-full'></div>
+}
+
+export default SearchBar
