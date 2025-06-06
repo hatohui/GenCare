c7be97e586caaf6943ff6dc179cbf82820b93930
Hồng Lê Đăng Khoa <hongkhoa348@gmail.com>
2025-06-06 22:43:22 +0700

    feat: fix viewPayLoad
    fix

--- backend/Application/DTOs/Service/Responses/ServicePayLoad.cs
+++ backend/Application/DTOs/Service/Responses/ServicePayLoad.cs
@@ -2,10 +2,9 @@
 
 public class ServicePayLoad
 {
-        public Guid Id { get; set; } 
-        public string? Name { get; set; } 
-        public string? Description { get; set; }
-        public decimal Price { get; set; }
-        public string ImageUrl { get; set; } = string.Empty; 
-    
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
 }
--- backend/Application/DTOs/Service/Responses/ViewServiceByPageResponse.cs
+++ backend/Application/DTOs/Service/Responses/ViewServiceByPageResponse.cs
@@ -4,7 +4,7 @@ public record class ViewServiceByPageResponse
 {
     public int Page { get; set; }
     public int Count { get; set; }
-    public List<ServicePayLoad> Payload { get; set; } 
+    public List<ServicePayLoad>? Payload { get; set; } 
     
 }
 
