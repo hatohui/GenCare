8759a34f78ba2f02b67c48074b36f32a1beba257
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2025-06-07 09:57:51 +0700

    fix: access Token

--- frontend/src/Utils/getAccessTokenHeader.ts
+++ frontend/src/Utils/getAccessTokenHeader.ts
@@ -3,15 +3,9 @@ import useToken from '@/Hooks/useToken'
 export const useAccessTokenHeader = (): string => {
 	const { accessToken } = useToken()
 
-	try {
-		if (!accessToken) {
-			throw new Error('Invalid or missing access Token')
-		}
-
-		return `Bearer ${accessToken}`
-	} catch (error) {
-		console.error(error)
-
+	if (!accessToken) {
 		return ''
 	}
+
+	return `Bearer ${accessToken}`
 }
