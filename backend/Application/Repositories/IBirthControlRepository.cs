3094c22f0e120487b545d096ad60034ad9bd174d
Hồng Lê Đăng Khoa <hongkhoa348@gmail.com>
2025-06-11 11:21:22 +0700

    feat: CRUD birthcontrol
    CRUD birthcontrol

---
+++ backend/Application/Repositories/IBirthControlRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Application.Repositories;
+
+public interface IBirthControlRepository
+{
+    Task<bool> AddBirthControlAsync(Guid birthControlId);
+    Task<bool> RemoveBirthControlAsync(Guid birthControlId);
+    Task<bool> UpdateBirthControlAsync(Guid birthControlId);
+    Task<BirthControl> GetBirthControlAsync(Guid birthControlId);
+}
---
+++ backend/Infrastructure/Repositories/BirthControlRepository.cs
@@ -0,0 +1,27 @@
+﻿using Application.Repositories;
+using Domain.Entities;
+
+namespace Infrastructure.Repositories;
+
+public class BirthControlRepository: IBirthControlRepository
+{
+    public Task<bool> AddBirthControlAsync(Guid birthControlId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> RemoveBirthControlAsync(Guid birthControlId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateBirthControlAsync(Guid birthControlId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BirthControl> GetBirthControlAsync(Guid birthControlId)
+    {
+        throw new NotImplementedException();
+    }
+}
