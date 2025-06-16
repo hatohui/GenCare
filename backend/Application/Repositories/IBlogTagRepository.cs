ff3138ac5e64543cfaf324ed15ce0ba625785386
Tiến Phát <Tienphat040322@gmail.com>
2025-06-16 15:48:36 +0700

    feat: add doc to repo

--- backend/Application/Repositories/IBlogTagRepository.cs
+++ backend/Application/Repositories/IBlogTagRepository.cs
@@ -1,16 +1,53 @@
-﻿
-using Domain.Entities;
+﻿using Domain.Entities;
 
 namespace Application.Repositories;
+/// <summary>
+/// Defines methods for managing <see cref="BlogTag"/> entities in the data store.
+/// </summary>
 public interface IBlogTagRepository
 {
+    /// <summary>
+    /// Retrieves all <see cref="BlogTag"/> entities from the data store.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation. The task result contains a list of blog tags.</returns>
     Task<List<BlogTag>> GetAll();
+
+    /// <summary>
+    /// Retrieves a <see cref="BlogTag"/> entity by its associated blog identifier.
+    /// </summary>
+    /// <param name="blogId">The unique identifier of the blog.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains the blog tag entity if found; otherwise, <c>null</c>.
+    /// </returns>
     Task<BlogTag?> GetByBlogId(string blogId);
+
+    /// <summary>
+    /// Retrieves a <see cref="BlogTag"/> entity by its associated tag identifier.
+    /// </summary>
+    /// <param name="tagId">The unique identifier of the tag.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains the blog tag entity if found; otherwise, <c>null</c>.
+    /// </returns>
     Task<BlogTag?> GetByTagId(string tagId);
-    //add
+
+    /// <summary>
+    /// Adds a new <see cref="BlogTag"/> to the data store.
+    /// </summary>
+    /// <param name="blogTag">The blog tag entity to add.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task Add(BlogTag blogTag);
-    //update
+
+    /// <summary>
+    /// Updates an existing <see cref="BlogTag"/> entity in the data store.
+    /// </summary>
+    /// <param name="blogTag">The blog tag entity to update.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task Update(BlogTag blogTag);
-    //delete
+
+    /// <summary>
+    /// Deletes a <see cref="BlogTag"/> entity from the data store.
+    /// </summary>
+    /// <param name="blogTag">The blog tag entity to delete.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task Delete(BlogTag blogTag);
 }
