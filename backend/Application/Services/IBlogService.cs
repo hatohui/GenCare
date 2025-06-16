0148fd6ca4873a13b1f0f861fe606c44752cf28c
Tiến Phát <Tienphat040322@gmail.com>
2025-06-16 17:44:20 +0700

    feat: blog create pending

--- backend/Application/Services/IBlogService.cs
+++ backend/Application/Services/IBlogService.cs
@@ -1,8 +1,10 @@
 ﻿
+using Application.DTOs.Blog.Request;
 using Application.DTOs.Blog.Response;
 
 namespace Application.Services;
 public interface IBlogService
 {
     Task<List<AllBlogViewResponse>> GetAllBlogsAsync();
+    Task AddBlogAsync(BlogCreateRequest request);
 }
