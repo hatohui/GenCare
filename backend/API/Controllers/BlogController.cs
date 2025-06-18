1a2b4b019303758b6e10f3e7359ee50c0adb316c
Tiến Phát <Tienphat040322@gmail.com>
2025-06-18 10:27:55 +0700

    feat: blog document

--- backend/API/Controllers/BlogController.cs
+++ backend/API/Controllers/BlogController.cs
@@ -3,14 +3,20 @@ using Application.Helpers;
 using Application.Services;
 using Domain.Common.Constants;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
+
+/// <summary>
+/// Controller for managing blog operations.
+/// </summary>
 [Route("api/[controller]")]
 [ApiController]
 public class BlogController(IBlogService blogService) : ControllerBase
 {
+    /// <summary>
+    /// Retrieves all blogs.
+    /// </summary>
+    /// <returns>A list of all blogs.</returns>
     [HttpGet]
     public async Task<IActionResult> GetAllBlogs()
     {
@@ -18,6 +24,11 @@ public class BlogController(IBlogService blogService) : ControllerBase
         return Ok(blogs);
     }
 
+    /// <summary>
+    /// Adds a new blog.
+    /// </summary>
+    /// <param name="request">The blog creation request data.</param>
+    /// <returns>A response indicating the result of the creation operation.</returns>
     [HttpPost]
     [Authorize(Roles = $"{RoleNames.Manager},{RoleNames.Admin},{RoleNames.Consultant},{RoleNames.Staff}")]
     public async Task<IActionResult> AddBlog([FromBody] BlogCreateRequest request)
@@ -30,6 +41,12 @@ public class BlogController(IBlogService blogService) : ControllerBase
         return Created();
     }
 
+    /// <summary>
+    /// Updates an existing blog.
+    /// </summary>
+    /// <param name="id">The ID of the blog to update.</param>
+    /// <param name="request">The blog update request data.</param>
+    /// <returns>A response indicating the result of the update operation.</returns>
     [HttpPut("{id}")]
     [Authorize(Roles = $"{RoleNames.Manager},{RoleNames.Admin},{RoleNames.Staff},{RoleNames.Consultant}")]
     public async Task<IActionResult> UpdateBlog([FromRoute] string id, [FromBody] BlogUpdateRequest request)
@@ -42,6 +59,11 @@ public class BlogController(IBlogService blogService) : ControllerBase
         return NoContent(); //204
     }
 
+    /// <summary>
+    /// Deletes a blog.
+    /// </summary>
+    /// <param name="id">The ID of the blog to delete.</param>
+    /// <returns>A response indicating the result of the delete operation.</returns>
     [HttpDelete("{id}")]
     [Authorize(Roles = $"{RoleNames.Manager},{RoleNames.Admin},{RoleNames.Staff},{RoleNames.Consultant}")]
     public async Task<IActionResult> DeleteBlog([FromRoute] string id)
