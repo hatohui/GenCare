39efb728acce755f6224f527c02e5bb5acc831fb
Tiến Phát <Tienphat040322@gmail.com>
2025-06-19 18:17:05 +0700

    feat: payment request

---
+++ backend/API/Controllers/PaymentController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Services;
+
+namespace API.Controllers;
+[Route("api/payment")]
+[ApiController]
+public class PaymentController(IMomoService momoService) : ControllerBase
+{
+    //api return url 
+    //
+    [HttpPost("momo-payment/{id}")]
+    public async Task<IActionResult> CreateMomoPayment([FromRoute] string id)
+    {
+        var result = await momoService.CreatePaymentAsync(id);
+        return Ok(new { payUrl = result.PayUrl, result });
+    }
+    [HttpPost("momo-callback")]
+    public IActionResult MomoNotify()
+    {
+        var response = momoService.ProcessPaymentCallback(Request.Query);
+        // Xử lý thông báo từ MoMo, cập nhật trạng thái đơn hàng trong database
+        return Ok(new { RspCode = "00", Message = "Success" });
+    }
+}
---
+++ backend/Application/DTOs/Payment/Momo/MomoPaymentResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.DTOs.Payment.Momo;
+public class MomoPaymentResponse
+{
+    public string PartnerCode { get; set; }
+    public string RequestId { get; set; }
+    public string OrderId { get; set; }
+    public decimal Amount { get; set; }
+    public long ResponseTime { get; set; }
+    public string Message { get; set; }
+    public string ResultCode { get; set; }
+    public string PayUrl { get; set; }
+    public string TransactionId { get; set; }
+    public string PaymentType { get; set; }
+    public string ResponseCode { get; set; }
+    public bool Success { get; set; }
+
+}
