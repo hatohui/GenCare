f92ab5c8f189cde8ac5273aad3ddc99a2eef9e73
Tiến Phát <Tienphat040322@gmail.com>
2025-06-22 21:56:29 +0700

    feat: payment

---
+++ backend/Application/Services/IPaymentHistoryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs.Payment;
+using Domain.Entities;
+
+namespace Application.Services;
+public interface IPaymentHistoryService
+{
+    Task CreatePaymentHistoryAsync(PaymentHistoryModel paymentHistory);
+}
---
+++ backend/Infrastructure/Services/PaymentHistoryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs.Payment;
+using Application.Repositories;
+using Application.Services;
+using Domain.Entities;
+
+namespace Infrastructure.Services;
+public class PaymentHistoryService(IPaymentHistoryRepository paymentHistoryRepository) : IPaymentHistoryService
+{
+    public async Task CreatePaymentHistoryAsync(PaymentHistoryModel model)
+    {
+        PaymentHistory payment = new()
+        {
+            PurchaseId = Guid.Parse(model.PurchaseId),
+            TransactionId = Guid.Parse(model.TransactionId),
+            CreatedAt = DateTime.Now,
+            Amount = model.Amount,
+            ExpiredAt = null // Assuming no expiration for the payment history
+        };
+        await paymentHistoryRepository.Add(payment);
+    }
+}
