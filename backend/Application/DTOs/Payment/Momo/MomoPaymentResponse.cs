2e125fb2e0bf965a5e35526149b8b81c4d2d39a7
Tiến Phát <Tienphat040322@gmail.com>
2025-06-25 09:53:59 +0700

    feat: non nullable

--- backend/Application/DTOs/Payment/Momo/MomoPaymentResponse.cs
+++ backend/Application/DTOs/Payment/Momo/MomoPaymentResponse.cs
@@ -1,15 +1,15 @@
 ﻿namespace Application.DTOs.Payment.Momo;
 public class MomoPaymentResponse
 {
-    public string PartnerCode { get; set; }
-    public string OrderId { get; set; }
-    public string RequestId { get; set; }
+    public string PartnerCode { get; set; } = null!;
+    public string OrderId { get; set; } = null!;
+    public string RequestId { get; set; } = null!;
     public long Amount { get; set; }
     public long ResponseTime { get; set; }
-    public string Message { get; set; }
+    public string Message { get; set; } = null!;
     public int ResultCode { get; set; }
-    public string PayUrl { get; set; }
-    public string DeepLink { get; set; }
-    public string QrCodeUrl { get; set; }
+    public string PayUrl { get; set; } = null!;
+    public string DeepLink { get; set; } = null!;
+    public string QrCodeUrl { get; set; } = null!;
 }
 
