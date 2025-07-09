c340c95974856e6fcfa1cafbedaac065d6b11d65
Tiến Phát <Tienphat040322@gmail.com>
2025-07-09 20:41:30 +0700

    feat: vnpay

---
+++ backend/Application/DTOs/Payment/VNPay/VNPayConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Payment.VNPay;
+public class VNPayConfig
+{
+    public string Vnp_TmnCode { get; set; } = null!;
+    public string Vnp_HashSecret { get; set; } = null!;
+    public string Vnp_Url { get; set; } = null!;
+    public string Vnp_ReturnUrl { get; set; } = null!;
+}
---
+++ backend/Application/DTOs/Payment/VNPay/VNPayIpnRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Payment.VNPay;
+public class VNPayIpnRequest
+{
+    public string vnp_TmnCode { get; set; } = null!;
+    public decimal vnp_Amount { get; set; }
+    public string vnp_BankCode { get; set; } = null!;
+    public string? vnp_BankTranNo { get; set; }
+    public string? vnp_CardType { get; set; }
+    public string? vnp_PayDate { get; set; }
+    public string vnp_OrderInfo { get; set; } = null!;
+    public string vnp_TransactionNo { get; set; } = null!;
+    public string vnp_ResponseCode { get; set; } = null!;
+    public string vnp_TransactionStatus { get; set; } = null!;
+    public string vnp_TxnRef { get; set; } = null!;
+    public string vnp_SecureHash { get; set; } = null!;
+}
