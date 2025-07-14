84bd235f4d8f678e56a3efe3e1e1ba5eca98492c
Hồng Lê Đăng Khoa <hongkhoa348@gmail.com>
2025-07-14 14:24:33 +0700

    fix: done
    fix api, and new data response

---
+++ backend/Application/DTOs/Result/Response/BookedServiceForTestResponse.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.Purchase;
+
+namespace Application.DTOs.Result.Response;
+
+public class BookedServiceForTestResponse
+{
+    public List<BookedServiceModel> Result { get; set; } = new();
+    public int TotalCount { get; set; }
+}
